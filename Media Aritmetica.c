35aa4000ed573e49caad3b68ebb25b81e5e3da8a
Marcelo Pilati <109703603+marcelopilati@users.noreply.github.com>
2022-08-22 20:58:15 -0300

    first commit

---
+++ Media Aritmetica.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main(){
+
+
+        float lado, area;
+
+        printf("Escreva o lado do quadrado: ");
+        scanf("%f",&lado);
+
+        area = lado * lado;
+        printf("A area do quadrado e: %f", area);
+
+}
